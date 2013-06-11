c44bb52ef8543b6a84dca91acaef8e47152a3b7b
Tom White <tom@sixdozen.com>
2013-06-10 23:41:56 -0700

    initial 0.1.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.drib/blade "0.1.0-SNAPSHOT"
+(defproject net.drib/blade "0.1.0"
   :description "blade: leaflet from clojurescript"
   :url "https://github.com/dribnet/blade"
   :license {:name "Eclipse Public License"

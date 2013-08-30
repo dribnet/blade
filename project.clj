6f0222145c6a634b0c8c100b139c533f45bfec6d
Tom White <tom@sixdozen.com>
2013-08-29 23:38:16 -0700

    updated mrhyde dependency to 0.5.2

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [net.drib/mrhyde "0.5.0"]  ]
+                 [net.drib/mrhyde "0.5.2"]  ]
   :min-lein-version "2.0.0"
   :source-paths ["src/cljs"]
 
